2668ed2f5d4d74a9b8740c5927b549948edb073b
Jhony Grillet <grilletjhony@gmail.com>
2021-01-28 16:35:35 -0400

    First commit

---
+++ .vuepress/components/AvocadoPrice.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Avocoin</h1>
+    <h2>1 BTC -> {{ price }} 🥑</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvocadoPrice',
+  data() {
+    return {
+      price: 0
+    }
+  },
+  created() {
+    fetch('https://avocoinapp.herokuapp.com')
+    .then(response => response.json())
+    .then(data => {
+      this.price = data.price
+    })
+  }
+}
+</script>
---
+++ README.md
@@ -0,0 +1,10 @@
+
+<AvocadoPrice />
+
+Este es mi primera página cambiada usando [JAMStack][1]
+
+<ContentEntries />
+
+
+[1]:https://jamstack.org
+
